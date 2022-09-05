7a45991dd6a9cf01c742a4614a80142b07e8298e
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-09-04 20:52:45 -0400

    add leetcode 1929

---
+++ leetcode/1929-concatenation-of-array.js
@@ -0,0 +1,15 @@
+const getConcatenation = (nums) => {
+    const ans = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        ans[i] = nums[i];
+        ans[i + nums.length] = nums[i];
+    }
+
+    return ans;
+}
+
+// REFACTORED VERSION:
+// const getConcatenation = (nums) => {
+//     return [...nums, ...nums];
+// }
