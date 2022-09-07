b6b4f718870cc2d242d5807197afd552c5e6db3e
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-09-06 23:58:27 -0400

    add leetcode 1379

---
+++ leetcode/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,26 @@
+// TIME: O(n) - visit every tree node
+// SPACE: O(n) - for the call stack to store upcoming function calls
+const getTargetCopy = (original, cloned, target) => {
+    if (original === null || original === target) {
+        return cloned;
+    }
+
+    // if the target was found in the left subtree, return the cloned tree's node
+    const leftSideValue = getTargetCopy(original.left, cloned.left, target);
+    if (leftSideValue) {
+        return leftSideValue;
+    }
+
+    // if the target was NOT found in the left subtree, check the right subtree
+    return getTargetCopy(original.right, cloned.right, target);
+}
+
+// REFACTORED VERSION (subtracting extraCandies):
+// const getTargetCopy = (original, cloned, target) => {
+//     if (original === null || original === target) {
+//         return cloned;
+//     }
+//
+//     return getTargetCopy(original.left, cloned.left, target) ||
+//         getTargetCopy(original.right, cloned.right, target);
+// }
