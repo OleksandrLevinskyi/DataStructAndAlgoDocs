514c538633da71668d538bddb57ea94f06115495
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-10-17 23:32:45 -0400

    define binary search tree test plan

---
+++ data-structures/binary-search-tree/BinarySearchTree.test.js
@@ -0,0 +1,37 @@
+describe('binary search tree', () => {
+    describe('insert', () => {
+        it('inserts a node into an empty tree', () => {
+        });
+
+        it('arranges the values correctly', () => {
+        });
+    });
+
+    describe('remove', () => {
+        it('returns undefined if trying to remove a node from an empty tree', () => {
+        });
+
+        it('removes a single node from a tree', () => {
+        });
+
+        it('removes a node from a tree and rearranges the other nodes correctly', () => {
+        });
+    });
+
+    describe('find', () => {
+        it('returns undefined if there are no nodes', () => {
+        });
+
+        it('finds a node if it is a root of the tree', () => {
+
+        });
+
+        it('finds a node on the left side of the tree', () => {
+
+        });
+
+        it('finds a node on the right side of the tree', () => {
+
+        });
+    });
+});
