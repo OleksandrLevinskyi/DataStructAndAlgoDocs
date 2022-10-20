b865e43b0251df858129fe60db54c294430a5ca5
OleksandrLevinskyi <aleksandrlevinskyi@gmail.com>
2022-10-19 21:32:25 -0400

    add test cases for binary search tree insert

--- data-structures/binary-search-tree/BinarySearchTree.test.js
+++ data-structures/binary-search-tree/BinarySearchTree.test.js
@@ -1,10 +1,39 @@
+const {BinarySearchTree} = require("./BinarySearchTree");
+
 describe('binary search tree', () => {
     describe('insert', () => {
         it('inserts a node into an empty tree', () => {
+            const tree = new BinarySearchTree();
 
+            const result = tree.insert(1);
+
+            expect(result).toEqual(tree);
+            expect(result.root.value).toEqual(1);
+            expect(result.root.left).toEqual(null);
+            expect(result.root.right).toEqual(null);
         });
 
         it('arranges the values correctly', () => {
+            const tree = new BinarySearchTree();
+
+            tree.insert(5);
+            tree.insert(9);
+            tree.insert(10);
+            tree.insert(0);
+            const result = tree.insert(2);
+
+            expect(result).toEqual(tree);
+            expect(result.root.value).toEqual(5);
+            expect(result.root.left.value).toEqual(0);
+            expect(result.root.left.left).toEqual(null);
+            expect(result.root.left.right.value).toEqual(2);
+            expect(result.root.left.right.left).toEqual(null);
+            expect(result.root.left.right.right).toEqual(null);
+            expect(result.root.right.value).toEqual(9);
+            expect(result.root.right.left).toEqual(null);
+            expect(result.root.right.right.value).toEqual(10);
+            expect(result.root.right.right.left).toEqual(null);
+            expect(result.root.right.right.right).toEqual(null);
         });
     });
 
