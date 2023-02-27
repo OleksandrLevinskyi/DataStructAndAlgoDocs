193aa6822d2e0160967ec278e349f8d1fe6a9f4b
Oleksandr Levinskyi <72713236+OleksandrLevinskyi@users.noreply.github.com>
2023-02-26 21:18:21 -0500

    Update 217-contains-duplicate.js

--- leetcode/217-contains-duplicate.js
+++ leetcode/217-contains-duplicate.js
@@ -1,15 +1,31 @@
+// 217-contains-duplicate.js
 // TIME: O(n)
 // SPACE: O(n)
 const containsDuplicate = (nums) => {
-    const dict = {};
+    const set = new Set();
 
     for (let num of nums) {
-        if (dict[num]) {
+        if (set.has(num)) {
             return true;
         }
 
-        dict[num] = true;
+        set.add(num);
     }
 
     return false;
 }
+
+// an alternative solution:
+// TIME: O(nlogn)
+// SPACE: O(1)
+// const containsDuplicate = (nums) => {
+//     nums.sort((a, b) => a - b);
+
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i - 1] === nums[i]) {
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
