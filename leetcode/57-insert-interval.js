e9f35dc10ff9ddeca7b3f7554cb976f4290c5f66
Oleksandr Levinskyi <72713236+OleksandrLevinskyi@users.noreply.github.com>
2023-03-10 01:45:54 -0500

    Update 57-insert-interval.js

--- leetcode/57-insert-interval.js
+++ leetcode/57-insert-interval.js
@@ -6,11 +6,13 @@ const insert = (intervals, newInterval) => {
     let newIntervalEnd = newInterval[newInterval.length - 1];
     let i = 0;
 
+    // put all of the intervals before the new one into the result
     while (i < intervals.length && intervals[i][intervals[i].length - 1] < newIntervalStart) {
         result.push(intervals[i]);
         i++;
     }
 
+    // put the new interval in place
     while (i < intervals.length && intervals[i][0] <= newIntervalEnd) {
         newIntervalStart = Math.min(intervals[i][0], newIntervalStart);
         newIntervalEnd = Math.max(intervals[i][intervals[i].length - 1], newIntervalEnd);
@@ -19,6 +21,7 @@ const insert = (intervals, newInterval) => {
 
     result.push([newIntervalStart, newIntervalEnd]);
 
+    // put the other intervals into the result
     while (i < intervals.length){
         result.push(intervals[i]);
         i++;
