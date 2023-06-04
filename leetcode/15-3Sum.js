92f3ae7a2df4a4eae8dd1cc9f3c6ce4b37840984
Oleksandr Levinskyi <72713236+OleksandrLevinskyi@users.noreply.github.com>
2023-06-04 02:19:22 -0400

    Update 15-3Sum.js

--- leetcode/15-3Sum.js
+++ leetcode/15-3Sum.js
@@ -2,41 +2,36 @@
 // SPACE: O(1)
 const threeSum = (nums) => {
     const result = [];
-    let solution;
     let l;
     let r;
     let sum;
+    let solution;
 
     nums.sort((a, b) => a - b);
-
-    for (let i = 0; i < nums.length - 2; i++) {
+    
+    for (let i = 0; i < nums.length - 1; i++) {
+        // make sure number 1 is not duplicated
         if (i > 0 && nums[i] === nums[i - 1]) {
-            continue; // skip a value if it is the same as the previous num 1 to avoid duplicates for number 1
+            continue;
         }
 
         l = i + 1;
         r = nums.length - 1;
 
-        // use two pointers
-        while (l < r) {
+        while (l < r) { 
             sum = nums[i] + nums[l] + nums[r];
 
-            if (sum < 0) {
+            if (sum < 0) { 
                 l++;
             } else if (sum > 0) {
                 r--;
             } else {
                 solution = [nums[i], nums[l], nums[r]];
                 result.push(solution);
-                
-                // avoid duplicates for number 2
+
+                // move left pointer to a new value to avoid duplicate set of triplets
                 while (l < r && nums[l] === solution[1]) {
                     l++;
-                }
-                
-                // avoid duplicates for number 3
-                while (l < r && nums[r] === solution[2]) {
-                    r--;
                 }
             }
         }
