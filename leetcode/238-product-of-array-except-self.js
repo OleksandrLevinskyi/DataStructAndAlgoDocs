494c913c4fd9f13c9e7b6020c08979d6d20f5218
Oleksandr Levinskyi <72713236+OleksandrLevinskyi@users.noreply.github.com>
2023-06-05 22:20:06 -0400

    Update 238-product-of-array-except-self.js

--- leetcode/238-product-of-array-except-self.js
+++ leetcode/238-product-of-array-except-self.js
@@ -4,15 +4,15 @@ const productExceptSelf = (nums) => {
     const result = [1];
     let rightSideProduct = 1;
 
-    // calculate left side product for each element and store it in result array
+    // calculate the left-side product
     for (let i = 1; i < nums.length; i++) {
         result[i] = result[i - 1] * nums[i - 1];
     }
 
-    // calculate right side product without extra memory
-    for (let i = nums.length - 1; i >= 0; i--) {
+    // calculate the right-side product and update the result array
+    for (let i = nums.length - 2; i >= 0; i--) {
+        rightSideProduct *= nums[i + 1];
         result[i] *= rightSideProduct;
-        rightSideProduct *= nums[i];
     }
 
     return result;
